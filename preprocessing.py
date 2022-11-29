b6d41bf6d7d3d5421231c7d7f3df5a324d63042d
Adharsh Venkat <adharsh.venkat98@gmail.com>
2022-11-30 00:49:29 +0800

    Added preprocessing.py -> Load data, basic preprocessing & EDA

--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,31 @@
-# import dependeicies 
+# Importing dependencies
 import pandas as pd
-# from sklearn.model_selection import train_test_split
 
-# read dataset and create dataframe
-raw_data = pd.read_csv("./data/data.csv")
 
-print(raw_data.head(10))
+### Pre-processing and Exploratpry Data Analysis ### 
+
+# Reading data from csv and saving to a dataframe
+raw_data = pd.read_csv('./data/data.csv')
+
+# Data dimensions
+raw_data.shape
+
+# Data information
+raw_data.info()
+
+# Top 5 data entries
+raw_data.head()
+
+# Checking for missing data
+raw_data.isnull().sum()
+
+# Checking for data imbalance
+raw_data["Bankrupt?"].value_counts()
+
+# Checking for duplicate data 
+raw_data.duplicated().sum()
+
+# Drop net income flag as all entries are 1, giving no useful information for model training
+raw_data[' Net Income Flag'].value_counts()
+
+raw_data = raw_data.drop(' Net Income Flag',axis=1)
