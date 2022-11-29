1cfb89d9c4c87b4c085341dbfd223edc3de71629
Adharsh Venkat <adharsh.venkat98@gmail.com>
2022-11-30 02:18:12 +0800

    Adding train test split and storing data in separatecsv files

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 # Importing dependencies
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 ### Pre-processing and Exploratpry Data Analysis ### 
 
@@ -29,3 +30,24 @@ raw_data.duplicated().sum()
 raw_data[' Net Income Flag'].value_counts()
 
 raw_data = raw_data.drop(' Net Income Flag',axis=1)
+
+# Separating features and label
+label = raw_data.columns[0]
+
+features = raw_data.columns[1:]
+
+X, Y = raw_data[features], raw_data[label]
+
+# Split data 80-20 into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, random_state=0)
+
+print ('Training cases: %d\nTest cases: %d' % (X_train.shape[0], X_test.shape[0]))
+
+train_data = pd.concat((y_train,X_train),axis=1)
+
+test_data = pd.concat((y_test, X_test),axis=1)
+
+# Writing the training and testing data into seperate csv files
+train_data.to_csv('./data/train.csv', index=False)
+
+train_data.to_csv('./data/test.csv', index=False)
